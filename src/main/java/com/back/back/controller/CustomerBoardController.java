dcbc66e84dd46b0eb54d84e1ce8f24e2cbb991e2
licoma <lcm413453@gmail.com>
2024-05-20 15:54:30 +0900

    4

--- src/main/java/com/back/back/controller/CustomerBoardController.java
+++ src/main/java/com/back/back/controller/CustomerBoardController.java
@@ -2,16 +2,24 @@ package com.back.back.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.back.back.dto.request.customer.PostCustomerBoardCommentRequestDto;
 import com.back.back.dto.request.customer.PostCustomerBoardRequestDto;
 import com.back.back.dto.response.ResponseDto;
 import com.back.back.service.CustomerBoardService;
+import com.back.back.dto.response.customerboard.GetCustomerBoardListResponseDto;
+import com.back.back.dto.response.customerboard.GetCustomerBoardResponseDto;
+import com.back.back.dto.response.customerboard.GetSearchCustomerBoardListResponseDto;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -32,12 +40,24 @@ public class CustomerBoardController {
         return response;
     }
 
-    @PostMapping("/{designBoardNumber}/comment")
+    @PostMapping("/{customerBoardNumber}/comment")
     public ResponseEntity<ResponseDto> postCustomerBoardComment (
         @RequestBody @Valid PostCustomerBoardCommentRequestDto requestBody,
         @PathVariable("customerBoardNumber") int customerBoardNumber
     ) {
         ResponseEntity<ResponseDto> response = customerBoardService.postCustomerBoardComment(requestBody, customerBoardNumber);
+        return response;
+    }
+    @GetMapping("/list")
+    public ResponseEntity<? super GetCustomerBoardListResponseDto> getCustomerBoardList () {
+        ResponseEntity<? super GetCustomerBoardListResponseDto> response = customerBoardService.getCustomerBoardList();
+        return response;
+    }
+    @GetMapping("/list/search")
+    public ResponseEntity<? super GetSearchCustomerBoardListResponseDto> getSearchCustomerBoardList (
+        @RequestParam("word") String word
+    ) {
+        ResponseEntity<? super GetSearchCustomerBoardListResponseDto> response = customerBoardService.getSearchCustomerBoardList(word);
         return response;
     }
 }
--- src/main/java/com/back/back/service/CustomerBoardService.java
+++ src/main/java/com/back/back/service/CustomerBoardService.java
@@ -5,8 +5,13 @@ import org.springframework.http.ResponseEntity;
 import com.back.back.dto.request.customer.PostCustomerBoardCommentRequestDto;
 import com.back.back.dto.request.customer.PostCustomerBoardRequestDto;
 import com.back.back.dto.response.ResponseDto;
+import com.back.back.dto.response.customerboard.GetCustomerBoardListResponseDto;
+import com.back.back.dto.response.customerboard.GetCustomerBoardResponseDto;
+import com.back.back.dto.response.customerboard.GetSearchCustomerBoardListResponseDto;
 
 public interface CustomerBoardService {
     ResponseEntity<ResponseDto> postCustomerBoard(PostCustomerBoardRequestDto dto, String userId);
     ResponseEntity<ResponseDto> postCustomerBoardComment(PostCustomerBoardCommentRequestDto dto, int customerBoardNumber);
+    ResponseEntity<? super GetCustomerBoardListResponseDto> getBoardList();
+    ResponseEntity<? super GetSearchCustomerBoardListResponseDto> getSearchCustomerBoardList(String searchWord);
 }
