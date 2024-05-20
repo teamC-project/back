bb3aa09795bc763e23e635c6907f17c0c762e74f
kia0919 <john0256@naver.com>
2024-05-20 11:00:48 +0900

    2024.05.20 11:00

---
+++ src/main/java/com/back/back/dto/response/auth/SignInResponseDto.java
@@ -0,0 +1,29 @@
+package com.back.back.dto.response.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.back.back.dto.response.ResponseCode;
+import com.back.back.dto.response.ResponseDto;
+import com.back.back.dto.response.ResponseMessage;
+
+import lombok.Getter;
+
+@Getter
+public class SignInResponseDto extends ResponseDto {
+    
+    private String accessToken;
+    private int expires;
+
+    private SignInResponseDto(String accessToken) {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.accessToken = accessToken;
+        this.expires = 60 * 60;
+    }
+
+    public static ResponseEntity<SignInResponseDto> success(String accessToken) {
+        SignInResponseDto responseBody = new SignInResponseDto(accessToken);
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+
+}
