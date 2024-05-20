ced8d02e633b37130ce08490aa53badfe54b8922
kia0919 <john0256@naver.com>
2024-05-20 14:57:41 +0900

    2024.05.20 14:57

--- src/main/java/com/back/back/entity/UserEntity.java
+++ src/main/java/com/back/back/entity/UserEntity.java
@@ -1,6 +1,8 @@
 package com.back.back.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,6 +18,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserEntity {
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private String userId;
     private String userPassword;
     private String userEmail;
@@ -25,4 +28,6 @@ public class UserEntity {
     private String userCompanyName;
     private String userRole;
     private String joinPath;
+
+    public UserEntity
 }
