eebc1788e865f73fa29c17dd1052893e282fbb93
GODDONGHYUN <leenl1168@gmail.com>
2024-05-20 17:51:42 +0900

    2024-05-20 관리자 미완성

---
+++ src/main/java/com/back/back/common/object/QnaBoardListItem.java
@@ -0,0 +1,44 @@
+package com.back.back.common.object;
+
+import java.util.List;
+import com.back.back.common.util.ChangeDateFormatUtil;
+import com.back.back.entity.QnaBoardEntity;
+import java.util.ArrayList;
+import lombok.Getter;
+
+@Getter
+public class QnaBoardListItem {
+	private Integer qnaBoardNumber;
+	private Boolean qnaBoardStatus;
+	private String qnaBoardTItle;
+	private String qnaBoardContents;
+	private String qnaBoardWriterId;
+	private String qnaBoardWriteDateTime;
+	private Integer qnaBoardViewCount;
+	private String qnaBoardComment;
+
+	private QnaBoardListItem(QnaBoardEntity qnaBoardEntity) throws Exception {
+		String writeDateTime = ChangeDateFormatUtil.changeYYMMDD(qnaBoardEntity.getQnaBoardWriteDateTime());
+
+		String qnaBoardWriterId = qnaBoardEntity.getQnaBoardWriterId();
+		qnaBoardWriterId = qnaBoardWriterId.substring(0,1) + " +".repeat(qnaBoardWriterId.length() - 1);
+
+		this.qnaBoardNumber = qnaBoardEntity.getQnaBoardNumber();
+		this.qnaBoardStatus = qnaBoardEntity.getQnaBoardStatus();
+		this.qnaBoardTItle = qnaBoardEntity.getQnaBoardTitle();
+		this.qnaBoardWriterId = qnaBoardEntity.getQnaBoardWriterId();
+		this.qnaBoardWriteDateTime = writeDateTime;
+		this.qnaBoardViewCount = qnaBoardEntity.getQnaBoardViewCount();
+
+	}
+
+	public static List<QnaBoardListItem> getQnaBoardList(List<QnaBoardEntity>qnaBoardEntities) throws Exception {
+		List<QnaBoardListItem> qnaBoardList = new ArrayList<>();
+
+		for ( QnaBoardEntity qnaBoardEntity : qnaBoardEntities) {
+			QnaBoardListItem qnaBoardListItem = new QnaBoardListItem(qnaBoardEntity);
+				qnaBoardList.add(qnaBoardListItem);
+		}
+		return qnaBoardList;
+	}
+}
--- src/main/java/com/back/back/service/implimentation/QnaBoardServiceImplementation.java
+++ src/main/java/com/back/back/service/implimentation/QnaBoardServiceImplementation.java
@@ -1,10 +1,18 @@
 package com.back.back.service.implimentation;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.back.back.dto.request.qna.PostQnaBoardCommentRequestDto;
 import com.back.back.dto.request.qna.PostQnaBoardRequestDto;
+import com.back.back.dto.request.qna.PutQnaBoardRequestDto;
 import com.back.back.dto.response.ResponseDto;
+import com.back.back.dto.response.qnaboard.GetQnaBoardListResponseDto;
+import com.back.back.dto.response.qnaboard.GetQnaBoardResponseDto;
+import com.back.back.dto.response.qnaboard.GetSearchQnaBoardListResponseDto;
+import com.back.back.entity.QnaBoardEntity;
 import com.back.back.repository.QnaBoardRepository;
 import com.back.back.repository.UserRepository;
 import com.back.back.service.QnaBoardService;
@@ -17,18 +25,153 @@ public class QnaBoardServiceImplementation implements QnaBoardService {
 
 	private final QnaBoardRepository qnaBoardRepository;
 	private final UserRepository userRepository;
+
 	@Override
 	public ResponseEntity<ResponseDto> postQnaBoard(PostQnaBoardRequestDto dto, String userId) {
 
 		try {
+			boolean isExistUser = userRepository.existsByUserId(userId);
+			if(!isExistUser) return ResponseDto.authenticationFailed();
 
-			
+			QnaBoardEntity qnaBoardEntity = new QnaBoardEntity(dto, userId);;
+			qnaBoardRepository.save(qnaBoardEntity);
 
 		} catch(Exception exception) {
 			exception.printStackTrace();
 			return ResponseDto.databaseError();
 		}
+		return ResponseDto.success();
+	}
+
+	@Override
+	public ResponseEntity<? super GetQnaBoardListResponseDto> getQnaBoardList() {
+		try {
+
+			List<QnaBoardEntity> boardEntities = qnaBoardRepository.findByOrderByQnaBoardNumberDesc();
+			return GetQnaBoardListResponseDto.success(boardEntities);
+
+	} catch (Exception exception) {
+			exception.printStackTrace();
+			return ResponseDto.databaseError();
+	}
+	}
+
+	@Override
+	public ResponseEntity<? super GetSearchQnaBoardListResponseDto> getSearchQnaBoardList(String qnaSearchWord) {
+		try {
+
+			List<QnaBoardEntity> qnaBoardEntities = qnaBoardRepository.findByTitleContainsOrderByQnaBoardNumberDesc(qnaSearchWord);
+			return GetSearchQnaBoardListResponseDto.success(qnaBoardEntities);
+
+	} catch (Exception exception) {
+			exception.printStackTrace();
+			return ResponseDto.databaseError();
+	}
+	}
+
+	@Override
+	public ResponseEntity<? super GetQnaBoardResponseDto> getQnaBoard(int qnaBoardNumber) {
+		try {
+			
+			QnaBoardEntity qnaBoardEntity = qnaBoardRepository.findByQnaNumber(qnaBoardNumber);
+			if (qnaBoardEntity == null) return ResponseDto.noExistBoard();
+			return GetQnaBoardResponseDto.success(qnaBoardEntity); 
+
+		} catch (Exception exception) {
+			exception.printStackTrace();
+			return ResponseDto.databaseError();
+		}
+	}
+
+	@Override
+	public ResponseEntity<ResponseDto> increaseQnaViewCount(int qnaBoardNumber) {
+		try {
+
+			QnaBoardEntity boardEntity = qnaBoardRepository.findByQnaNumber(qnaBoardNumber);
+			if (boardEntity == null) return ResponseDto.noExistBoard();
+
+			boardEntity.increaseQnaViewCount();
+			qnaBoardRepository.save(boardEntity);
+
+	} catch (Exception exception) {
+			exception.printStackTrace();
+			return ResponseDto.databaseError();
+	}
+
+	return ResponseDto.success();
 
 	}
-	
-}
+
+	@Override
+	public ResponseEntity<ResponseDto> putQnaBoard(PutQnaBoardRequestDto dto, int qnaBoardNumber, String userId) {
+		try {
+
+			QnaBoardEntity qnaBoardEntity = qnaBoardRepository.findByQnaNumber(qnaBoardNumber);
+			if (qnaBoardEntity == null) return ResponseDto.noExistBoard();
+
+			String writerId = qnaBoardEntity.getQnaBoardWriterId();
+			boolean isWriter = userId.equals(writerId);
+			if (!isWriter) return ResponseDto.authorizationFailed();
+
+			boolean status = qnaBoardEntity.getQnaBoardStatus();
+			if (status) return ResponseDto.writtenComment();
+
+			qnaBoardEntity.updateQnaBoard(dto);
+			qnaBoardRepository.save(qnaBoardEntity);
+
+	} catch (Exception exception) {
+			exception.printStackTrace();
+			return ResponseDto.databaseError();
+	}
+
+	return ResponseDto.success();
+	}
+
+	@Override
+	public ResponseEntity<ResponseDto> postQnaBoardComment(PostQnaBoardCommentRequestDto dto, int qnaBoardNumber) {
+		try {
+
+			QnaBoardEntity qnaBoardEntity = qnaBoardRepository.findByQnaNumber(qnaBoardNumber);
+			if (qnaBoardEntity == null) return ResponseDto.noExistBoard();
+
+			boolean qnaBoardStatus = qnaBoardEntity.getQnaBoardStatus();
+			if (qnaBoardStatus) return ResponseDto.writtenComment();
+
+			String qnaBoardComment = dto.getQnaBoardComment();
+			qnaBoardEntity.setQnaBoardStatus(true);
+			qnaBoardEntity.setQnaBoardComment(qnaBoardComment);
+
+			qnaBoardRepository.save(qnaBoardEntity);
+
+	} catch (Exception exception) {
+			exception.printStackTrace();
+			return ResponseDto.databaseError();
+	}
+
+	return ResponseDto.success();
+	}
+
+	@Override
+	public ResponseEntity<ResponseDto> deleteQnaBoard(int qnaBoardNumber, String userId) {
+		try {
+
+			QnaBoardEntity qnaBoardEntity = qnaBoardRepository.findByQnaNumber(qnaBoardNumber);
+			if (qnaBoardEntity == null) return ResponseDto.noExistBoard();
+
+			String qnaBoardWriterId = qnaBoardEntity.getQnaBoardWriterId();
+			boolean isQnaWriter = userId.equals(qnaBoardWriterId);
+			if (!isQnaWriter) return ResponseDto.authorizationFailed();
+
+			qnaBoardRepository.delete(qnaBoardEntity);
+
+	} catch (Exception exception) {
+			exception.printStackTrace();
+			return ResponseDto.databaseError();
+	}
+
+	return ResponseDto.success();
+	}
+
+
+
+	}
