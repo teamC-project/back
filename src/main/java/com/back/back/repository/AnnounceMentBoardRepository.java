e60fcae061c0d2246d27284c0fa5859621ac97b8
ahndb <ahndb.dev@gmail.com>
2024-05-21 16:12:58 +0900

    1

--- src/main/java/com/back/back/repository/AnnounceMentBoardRepository.java
+++ src/main/java/com/back/back/repository/AnnounceMentBoardRepository.java
@@ -9,17 +9,9 @@ import com.back.back.entity.AnnouncementBoardEntity;
 
 
 @Repository
-<<<<<<< HEAD
 public interface AnnouncementBoardRepository extends JpaRepository <AnnouncementBoardEntity, Integer> {
 
     List<AnnouncementBoardEntity> findByOrderByAnnouncementBoardNumberDesc();
     List<AnnouncementBoardEntity> findByAnnouncementBoardTitleOrderByAnnouncementBoardNumberDesc(String announcementBoardTitle);
     AnnouncementBoardEntity findByAnnouncementBoardNumber(Integer announcementBoardNumber);
-=======
-public interface AnnounceMentBoardRepository extends JpaRepository <AnnouncementBoardEntity, Integer> {
-
-    List<AnnouncementBoardEntity> findByOrderByAnnouncementNumberDesc();
-    List<AnnouncementBoardEntity> findByTitleContainsOrderByAnnouncementBoardNumberDesc(String announceMentTitle);
-    AnnouncementBoardEntity findByAnnouncementBoardNumber(Integer announceMentNumber);
->>>>>>> 44fca5349da5c0d03521ca57b33ed5892a9027a6
 }
