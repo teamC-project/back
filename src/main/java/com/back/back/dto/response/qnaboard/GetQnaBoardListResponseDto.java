7591d29c1f50fcad24af5b540f8b0d70895ffce6
licoma <lcm413453@gmail.com>
2024-05-21 12:32:57 +0900

    4

--- src/main/java/com/back/back/dto/response/qnaboard/GetQnaBoardListResponseDto.java
+++ src/main/java/com/back/back/dto/response/qnaboard/GetQnaBoardListResponseDto.java
@@ -1,29 +1,29 @@
-package com.back.back.dto.response.qnaboard;
+// package com.back.back.dto.response.qnaboard;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
 
-import com.back.back.common.object.QnaBoardListItem;
-import com.back.back.dto.response.ResponseCode;
-import com.back.back.dto.response.ResponseDto;
-import com.back.back.dto.response.ResponseMessage;
-import com.back.back.entity.QnaBoardEntity;
+// import com.back.back.common.object.QnaBoardListItem;
+// import com.back.back.dto.response.ResponseCode;
+// import com.back.back.dto.response.ResponseDto;
+// import com.back.back.dto.response.ResponseMessage;
+// import com.back.back.entity.QnaBoardEntity;
 
-import lombok.Getter;
+// import lombok.Getter;
 
-@Getter
-public class GetQnaBoardListResponseDto extends ResponseDto {
-	private List<QnaBoardListItem> qnaBoardList;
+// @Getter
+// public class GetQnaBoardListResponseDto extends ResponseDto {
+// 	private List<QnaBoardListItem> qnaBoardList;
 
-	private GetQnaBoardListResponseDto(List<QnaBoardEntity> qnaBoardEntities) throws Exception {
-		super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
-		this.qnaBoardList = QnaBoardListItem.getQnaBoardList(qnaBoardEntities);
-	}
+// 	private GetQnaBoardListResponseDto(List<QnaBoardEntity> qnaBoardEntities) throws Exception {
+// 		super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+// 		this.qnaBoardList = QnaBoardListItem.getQnaBoardList(qnaBoardEntities);
+// 	}
 
-	public static ResponseEntity<GetQnaBoardListResponseDto> success(List<QnaBoardEntity> qnaBoardEntities) throws Exception{
-							GetQnaBoardListResponseDto responseBody = new GetQnaBoardListResponseDto(qnaBoardEntities);
-					return ResponseEntity.status(HttpStatus.OK).body(responseBody);
-	}
-	}
+// 	public static ResponseEntity<GetQnaBoardListResponseDto> success(List<QnaBoardEntity> qnaBoardEntities) throws Exception{
+// 							GetQnaBoardListResponseDto responseBody = new GetQnaBoardListResponseDto(qnaBoardEntities);
+// 					return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+// 	}
+// 	}
--- src/main/java/com/back/back/dto/response/qnaboard/GetQnaBoardResponseDto.java
+++ src/main/java/com/back/back/dto/response/qnaboard/GetQnaBoardResponseDto.java
@@ -1,44 +1,44 @@
-package com.back.back.dto.response.qnaboard;
+// package com.back.back.dto.response.qnaboard;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
 
-import com.back.back.common.util.ChangeDateFormatUtil;
-import com.back.back.dto.response.ResponseCode;
-import com.back.back.dto.response.ResponseDto;
-import com.back.back.dto.response.ResponseMessage;
-import com.back.back.entity.QnaBoardEntity;
-import com.back.back.dto.response.qnaboard.GetQnaBoardResponseDto;
+// import com.back.back.common.util.ChangeDateFormatUtil;
+// import com.back.back.dto.response.ResponseCode;
+// import com.back.back.dto.response.ResponseDto;
+// import com.back.back.dto.response.ResponseMessage;
+// import com.back.back.entity.QnaBoardEntity;
+// import com.back.back.dto.response.qnaboard.GetQnaBoardResponseDto;
 
-import lombok.Getter;
+// import lombok.Getter;
 
-@Getter
-public class GetQnaBoardResponseDto extends ResponseDto{
-	private Integer qnaBoardNumber;
-	private Boolean qnaBoardStatus;
-	private String qnaBoardTItle;
-	private String qnaBoardContents;
-	private String qnaBoardWriterId;
-	private String qnaBoardWriteDateTime;
-	private Integer qnaBoardViewCount;
-	private String qnaBoardComment;
+// @Getter
+// public class GetQnaBoardResponseDto extends ResponseDto{
+// 	private Integer qnaBoardNumber;
+// 	private Boolean qnaBoardStatus;
+// 	private String qnaBoardTItle;
+// 	private String qnaBoardContents;
+// 	private String qnaBoardWriterId;
+// 	private String qnaBoardWriteDateTime;
+// 	private Integer qnaBoardViewCount;
+// 	private String qnaBoardComment;
 
-	private GetQnaBoardResponseDto(QnaBoardEntity qnaBoardEntity) throws Exception {
-		super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
-		String writeDateTime = ChangeDateFormatUtil.changeYYYYMMDD(qnaBoardEntity.getQnaBoardWriteDateTime());
+// 	private GetQnaBoardResponseDto(QnaBoardEntity qnaBoardEntity) throws Exception {
+// 		super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+// 		String writeDateTime = ChangeDateFormatUtil.changeYYYYMMDD(qnaBoardEntity.getQnaBoardWriteDateTime());
 
-		this.qnaBoardNumber = qnaBoardEntity.getQnaBoardNumber();
-		this.qnaBoardStatus = qnaBoardEntity.getQnaBoardStatus();
-		this.qnaBoardTItle = qnaBoardEntity.getQnaBoardTitle();
-		this.qnaBoardWriterId = qnaBoardEntity.getQnaBoardWriterId();
-		this.qnaBoardWriteDateTime = writeDateTime;
-		this.qnaBoardViewCount = qnaBoardEntity.getQnaBoardViewCount();
-		this.qnaBoardContents = qnaBoardEntity.getQnaBoardContents();
-		this.qnaBoardComment = qnaBoardEntity.getQnaBoardComment();
-	}
+// 		this.qnaBoardNumber = qnaBoardEntity.getQnaBoardNumber();
+// 		this.qnaBoardStatus = qnaBoardEntity.getQnaBoardStatus();
+// 		this.qnaBoardTItle = qnaBoardEntity.getQnaBoardTitle();
+// 		this.qnaBoardWriterId = qnaBoardEntity.getQnaBoardWriterId();
+// 		this.qnaBoardWriteDateTime = writeDateTime;
+// 		this.qnaBoardViewCount = qnaBoardEntity.getQnaBoardViewCount();
+// 		this.qnaBoardContents = qnaBoardEntity.getQnaBoardContents();
+// 		this.qnaBoardComment = qnaBoardEntity.getQnaBoardComment();
+// 	}
 
-	public static ResponseEntity<GetQnaBoardResponseDto> success (QnaBoardEntity qnaBoardEntity) throws Exception {
-		GetQnaBoardResponseDto responseBody = new GetQnaBoardResponseDto(qnaBoardEntity);
-		return ResponseEntity.status(HttpStatus.OK).body(responseBody);
-	}
-}
+// 	public static ResponseEntity<GetQnaBoardResponseDto> success (QnaBoardEntity qnaBoardEntity) throws Exception {
+// 		GetQnaBoardResponseDto responseBody = new GetQnaBoardResponseDto(qnaBoardEntity);
+// 		return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+// 	}
+// }
