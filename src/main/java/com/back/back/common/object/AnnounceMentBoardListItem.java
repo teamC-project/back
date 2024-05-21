0ff2231bd114a43e341023d1199c974ebba1ee98
licoma <lcm413453@gmail.com>
2024-05-21 16:13:10 +0900

    7

--- src/main/java/com/back/back/common/object/AnnounceMentBoardListItem.java
+++ src/main/java/com/back/back/common/object/AnnounceMentBoardListItem.java
@@ -8,17 +8,6 @@ import lombok.Getter;
 
 @Getter
 public class AnnouncementBoardListItem {
-<<<<<<< HEAD
-	private Integer announceMentBoardNumber;
-	private String announceMentBoardTitle;
-	private String announceMentBoardContents;
-	private String announceMentBoardWriterId;
-	private String announceMentBoardWriteDateTime;
-	private Integer announceMentBoardViewCount;
-
-private AnnouncementBoardListItem(AnnouncementBoardEntity announcementBoardEntity) throws Exception {
-	String writeDatetime = ChangeDateFormatUtil.changeYYMMDD(announcementBoardEntity.getAnnounceMentBoardWriteDateTime());
-=======
 	private Integer announcementBoardNumber;
 	private String announcementBoardTitle;
 	private String announcementBoardContents;
@@ -28,7 +17,6 @@ private AnnouncementBoardListItem(AnnouncementBoardEntity announcementBoardEntit
 
 private AnnouncementBoardListItem(AnnouncementBoardEntity announcementBoardEntity) throws Exception {
 	String writeDatetime = ChangeDateFormatUtil.changeYYMMDD(announcementBoardEntity.getAnnouncementBoardWriteDateTime());
->>>>>>> 97a21643a440a31601363b460738b873ee41fbf9
 
 	String announcementBoardWriterId = announcementBoardEntity.getAnnouncementBoardWriterId();
 	announcementBoardWriterId = announcementBoardWriterId.substring(0,1) + " +".repeat(announcementBoardWriterId.length() -1);
@@ -42,19 +30,11 @@ private AnnouncementBoardListItem(AnnouncementBoardEntity announcementBoardEntit
 }
 
 public static List<AnnouncementBoardListItem> getAnnouncementBoardList(List<AnnouncementBoardEntity> announcementBoardEntities) throws Exception {
-<<<<<<< HEAD
-	List<AnnouncementBoardListItem> announceMentBoardList = new ArrayList<>();
-
-	for (AnnouncementBoardEntity announcementBoardEntity : announcementBoardEntities) {
-		AnnouncementBoardListItem announceMentBoardListItem = new AnnouncementBoardListItem(announcementBoardEntity);
-		announceMentBoardList.add(announceMentBoardListItem);
-=======
 	List<AnnouncementBoardListItem> announcementBoardList = new ArrayList<>();
 
 	for (AnnouncementBoardEntity announcementBoardEntity : announcementBoardEntities) {
 		AnnouncementBoardListItem announcementBoardListItem = new AnnouncementBoardListItem(announcementBoardEntity);
 		announcementBoardList.add(announcementBoardListItem);
->>>>>>> 97a21643a440a31601363b460738b873ee41fbf9
 
 	}
 	return announcementBoardList;
