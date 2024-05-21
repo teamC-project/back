bfd2ab0a691fe4312610dc9b969f821b6f5a640a
kia0919 <john0256@naver.com>
2024-05-21 17:52:24 +0900

    17:52

--- src/main/java/com/back/back/dto/request/auth/FindPasswordDto.java
+++ src/main/java/com/back/back/dto/request/auth/FindPasswordDto.java
@@ -1,6 +1,7 @@
 package com.back.back.dto.request.auth;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,7 +11,10 @@ import lombok.Setter;
 @NoArgsConstructor
 public class FindPasswordDto {
   @NotBlank
-  public String userEmail;
-  @NotBlank
   public String authNumber;
+  @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,15}$")
+    private String userPassword;
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9]*@([-.]?[a-zA-Z0-9])*\\.[a-zA-Z]{2,4}$")
+    private String userEmail;
 }
