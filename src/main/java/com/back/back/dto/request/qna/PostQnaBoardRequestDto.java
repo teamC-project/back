0ba7b0c746a0843f59ce3233bfab305471880cff
heesanggod <gmltkd263@naver.com>
2024-05-22 09:21:12 +0900

    1

--- src/main/java/com/back/back/dto/request/qna/PostQnaBoardRequestDto.java
+++ src/main/java/com/back/back/dto/request/qna/PostQnaBoardRequestDto.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package com.back.back.dto.request.qna;
 
 import jakarta.validation.constraints.NotBlank;
@@ -16,36 +14,3 @@ public class PostQnaBoardRequestDto {
 	@NotBlank
 	public String qnaBoardContents;
 }
-=======
-// package com.back.back.dto.request.qna;
-=======
-package com.back.back.dto.request.qna;
->>>>>>> 65f1d1f75adfa11dbdad945be3dc4578883613fd
-
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-<<<<<<< HEAD
-// @Getter
-// @Setter
-// @NoArgsConstructor
-// public class PostQnaBoardRequestDto {
-// 	@NotBlank
-// 	public String qnaBoardTitle;
-// 	@NotBlank
-// 	public String qnaBoardContents;
-// }
->>>>>>> 3e5392899efc054d691836d3d05a40deb2a1161f
-=======
-@Getter
-@Setter
-@NoArgsConstructor
-public class PostQnaBoardRequestDto {
-	@NotBlank
-	public String qnaBoardTitle;
-	@NotBlank
-	public String qnaBoardContents;
-}
->>>>>>> 65f1d1f75adfa11dbdad945be3dc4578883613fd
