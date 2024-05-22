fd9a0ae1c09b959c838d4f53eb82792e0133324b
licoma <lcm413453@gmail.com>
2024-05-22 09:20:41 +0900

    4

--- src/main/java/com/back/back/dto/request/qna/PostQnaBoardCommentRequestDto.java
+++ src/main/java/com/back/back/dto/request/qna/PostQnaBoardCommentRequestDto.java
@@ -1,31 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-package com.back.back.dto.request.qna;
-
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-public abstract class PostQnaBoardCommentRequestDto {
-@NotBlank
-public String qnaBoardComment;
-}
-=======
+// <<<<<<< HEAD
+// <<<<<<< HEAD
 // package com.back.back.dto.request.qna;
-=======
-package com.back.back.dto.request.qna;
->>>>>>> 65f1d1f75adfa11dbdad945be3dc4578883613fd
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+// import jakarta.validation.constraints.NotBlank;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
 
-<<<<<<< HEAD
 // @Getter
 // @Setter
 // @NoArgsConstructor
@@ -33,13 +14,32 @@ import lombok.Setter;
 // @NotBlank
 // public String qnaBoardComment;
 // }
->>>>>>> 3e5392899efc054d691836d3d05a40deb2a1161f
-=======
-@Getter
-@Setter
-@NoArgsConstructor
-public abstract class PostQnaBoardCommentRequestDto {
-@NotBlank
-public String qnaBoardComment;
-}
->>>>>>> 65f1d1f75adfa11dbdad945be3dc4578883613fd
+// =======
+// // package com.back.back.dto.request.qna;
+// =======
+// package com.back.back.dto.request.qna;
+// >>>>>>> 65f1d1f75adfa11dbdad945be3dc4578883613fd
+
+// import jakarta.validation.constraints.NotBlank;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
+
+// <<<<<<< HEAD
+// // @Getter
+// // @Setter
+// // @NoArgsConstructor
+// // public abstract class PostQnaBoardCommentRequestDto {
+// // @NotBlank
+// // public String qnaBoardComment;
+// // }
+// >>>>>>> 3e5392899efc054d691836d3d05a40deb2a1161f
+// =======
+// @Getter
+// @Setter
+// @NoArgsConstructor
+// public abstract class PostQnaBoardCommentRequestDto {
+// @NotBlank
+// public String qnaBoardComment;
+// }
+// >>>>>>> 65f1d1f75adfa11dbdad945be3dc4578883613fd
--- src/main/java/com/back/back/repository/QnaBoardRepository.java
+++ src/main/java/com/back/back/repository/QnaBoardRepository.java
@@ -14,12 +14,9 @@
 // QnaBoardEntity findByQnaNumber(Integer qnaBoardNumber);
 
 // }
-<<<<<<< HEAD
-=======
 
 // // List<QnaBoardEntity> findByOrderByQnaBoardNumberDesc();
 // // List<QnaBoardEntity> findByTitleContainsOrderByQnaBoardNumberDesc(String qnaTitle);
 // // QnaBoardEntity findByQnaNumber(Integer qnaBoardNumber);
 // // }
 
->>>>>>> 97c62b7b9db2154356dd05eedaa9b3680e541b98
