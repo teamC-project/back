5c2ce6fcd11b7d2e4d9b24704bb0910554d3782d
heesanggod <gmltkd263@naver.com>
2024-05-24 17:22:16 +0900

    1

---
+++ src/main/java/com/back/back/controller/DesignerBoardImageController.java
@@ -0,0 +1,38 @@
+package com.back.back.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.back.back.service.DesignerBoardImageService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/designer_board")
+@RequiredArgsConstructor
+public class DesignerBoardImageController {
+    private final DesignerBoardImageService designerBoardImageService;
+
+    @PostMapping("/upload")
+    public String upload(
+		@RequestParam("file") MultipartFile file
+	) {
+		String url = designerBoardImageService.upload(file);
+		return url;
+	}
+
+    @GetMapping(value="/file/{fileName}", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
+	public Resource getFile(
+		@PathVariable("fileName") String fileName
+	) {
+		Resource resource = designerBoardImageService.getFile(fileName);
+		return resource;
+	}
+}
