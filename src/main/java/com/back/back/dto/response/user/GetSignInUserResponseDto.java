b3452c5afbfaa685cf6c6f6fed30a256c8c928fb
kia0919 <john0256@naver.com>
2024-06-10 17:13:39 +0900

    개인정보수정화면 정보 띄우기

--- src/main/java/com/back/back/dto/response/user/GetSignInUserResponseDto.java
+++ src/main/java/com/back/back/dto/response/user/GetSignInUserResponseDto.java
@@ -14,11 +14,22 @@ import lombok.Getter;
 public class GetSignInUserResponseDto extends ResponseDto {
 
   private String userId;
+  private String userEmail;
+  private String userGender;
+  private String userAge;
+  private String userImage;
+  private String userCompanyName;
   private String userRole;
+  
 
   private GetSignInUserResponseDto(UserEntity userEntity) {
     super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
     this.userId = userEntity.getUserId();
+    this.userEmail = userEntity.getUserEmail();
+    this.userGender = userEntity.getUserGender();
+    this.userAge = userEntity.getUserAge();
+    this.userImage = userEntity.getUserImage();
+    this.userCompanyName = userEntity.getUserCompanyName();
     this.userRole = userEntity.getUserRole();
   }
   
