95f8ee1edd2b1e7c2ade4acb5996b512573cce8c
kia0919 <john0256@naver.com>
2024-06-18 17:19:23 +0900

    마이페이지 - 디자이너 개인정보수정 찐완

--- src/main/java/com/back/back/dto/response/ResponseDto.java
+++ src/main/java/com/back/back/dto/response/ResponseDto.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.back.back.dto.response.chatmessage.GetChatMessageListResponseDto;
-import com.back.back.dto.response.chatroom.GetChatroomListResponseDto;
 import com.back.back.dto.response.chatroom.GetChatroomResponseDto;
 
 import lombok.AllArgsConstructor;
@@ -14,94 +13,94 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ResponseDto {
   private String code;
-  private String message;
+  private String message;  
 
       public static ResponseEntity<ResponseDto> success() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> validatyionFailed() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.VALIDATION_FAILED, ResponseMessage.VALIDATION_FAILED);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> duplicatedId() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.DUPLICATED_ID, ResponseMessage.DUPLICATED_ID);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> duplicatedEmail() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.DUPLICATED_EMAIL, ResponseMessage.DUPLICATED_EMAIL);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> noExistBoard() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.NO_EXIST_BOARD, ResponseMessage.NO_EXIST_BOARD);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> noExistEmail() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.NO_EXIST_EMAIL, ResponseMessage.NO_EXIST_EMAIL);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> noExistId() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.NO_EXIST_ID, ResponseMessage.NO_EXIST_ID);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> writtenComment() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.WRITTEN_COMMENT, ResponseMessage.WRITTEN_COMMENT);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> signInFailed() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.SIGN_IN_FAILED, ResponseMessage.SIGN_IN_FAILED);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> authenticationFailed() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.AUTHENTICATION_FAILED, ResponseMessage.AUTHENTICATION_FAILED);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> authorizationFailed() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.AUTHORIZATION_FAILED, ResponseMessage.AUTHORIZATION_FAILED);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> pageNotFound() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.VALIDATION_FAILED, ResponseMessage.VALIDATION_FAILED);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> tokenCreationFailed() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.TOKEN_CREATION_FAILED, ResponseMessage.TOKEN_CREATION_FAILED);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> mailSendFailed() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.MAIL_SEND_FAILED, ResponseMessage.MAIL_SEND_FAILED);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
     }
 
       public static ResponseEntity<ResponseDto> databaseError() {
-        ResponseDto responseBody =
+        ResponseDto responseBody = 
             new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
     }
@@ -111,13 +110,8 @@ public class ResponseDto {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
     
-
-    public static ResponseEntity<GetChatroomResponseDto> invalidRoomIdForGetChatroom() {
-      GetChatroomResponseDto responseBody = new GetChatroomResponseDto(false, null);
-
     public static ResponseEntity<? super GetChatroomResponseDto> invalidRoomIdForGetChatroom() {
       ResponseDto responseBody = new ResponseDto(ResponseCode.INVALID_ROOM_ID, ResponseMessage.INVALID_ROOM_ID); 
-
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
   }
   
