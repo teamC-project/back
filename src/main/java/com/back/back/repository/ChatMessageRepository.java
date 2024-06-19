ee6c74cd3f7ef227dc34822e56fb0b0ba3c788ae
heesanggod <gmltkd263@naver.com>
2024-06-19 14:25:38 +0900

    1

--- src/main/java/com/back/back/repository/ChatMessageRepository.java
+++ src/main/java/com/back/back/repository/ChatMessageRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.back.back.entity.ChatMessageEntity;
-import com.back.back.entity.ChatroomEntity;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessageEntity, Integer> {
