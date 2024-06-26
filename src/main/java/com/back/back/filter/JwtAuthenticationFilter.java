24638dd5204b94db44c34d589f2d2696203147f0
ahndb <ahndb.dev@gmail.com>
2024-06-26 12:43:12 +0900

    Update filter

--- src/main/java/com/back/back/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/back/back/filter/JwtAuthenticationFilter.java
@@ -1,19 +1,19 @@
 package com.back.back.filter;
 
+import java.util.List;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.util.StringUtils;
+import org.springframework.stereotype.Component;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import com.back.back.entity.UserEntity;
 import com.back.back.provider.JwtProvider;
@@ -23,6 +23,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -40,27 +41,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         String token = parseBearerToken(request);
         if (token == null) {
-        filterChain.doFilter(request, response);
-        return;
+            filterChain.doFilter(request, response);
+            return;
         }
 
         String userId = jwtProvider.validate(token);
         if (userId == null) {
-        filterChain.doFilter(request, response);
-        return;
+            filterChain.doFilter(request, response);
+            return;
         }
 
         UserEntity userEntity = userRepository.findByUserId(userId);
         if (userEntity == null) {
-        filterChain.doFilter(request, response);
-        return;
+            filterChain.doFilter(request, response);
+            return;
         }
 
         String role = userEntity != null ? userEntity.getUserRole() : null;
 
         List<GrantedAuthority> authorities = new ArrayList<>();
         if (role != null) {
-        authorities.add(new SimpleGrantedAuthority(role));
+            authorities.add(new SimpleGrantedAuthority(role));
         }
 
         AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userId, null, authorities);
@@ -75,24 +76,24 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         exception.printStackTrace();
     }
 
-    filterChain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
 
     private String parseBearerToken(HttpServletRequest request) {
 
-    String authoization = request.getHeader("Authorization");
-    boolean hasAuthorization = StringUtils.hasText(authoization);
+        String authoization = request.getHeader("Authorization");
+        boolean hasAuthorization = StringUtils.hasText(authoization);
 
-    if (!hasAuthorization)
-        return null;
+        if (!hasAuthorization)
+            return null;
 
-    boolean isBearer = authoization.startsWith("Bearer ");
+        boolean isBearer = authoization.startsWith("Bearer ");
 
-    if (!isBearer)
-        return null;
+        if (!isBearer)
+            return null;
 
-    String token = authoization.substring(7);
-    return token;
+        String token = authoization.substring(7);
+        return token;
     }
 
 }
