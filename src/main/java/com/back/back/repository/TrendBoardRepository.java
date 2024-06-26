aa8750d4401548708cab49aea615109543215c82
GODDONGHYUN <leenl1168@gmail.com>
2024-06-26 10:51:57 +0900

    204-06-26

--- src/main/java/com/back/back/repository/TrendBoardRepository.java
+++ src/main/java/com/back/back/repository/TrendBoardRepository.java
@@ -9,12 +9,6 @@ import com.back.back.entity.TrendBoardEntity;
 
 @Repository
 public interface TrendBoardRepository extends JpaRepository<TrendBoardEntity, Integer> {
-<<<<<<< HEAD
-	List<TrendBoardEntity> findByOrderByTrendBoardNumberDesc();
-	List<TrendBoardEntity> findByTrendBoardTitleOrderByTrendBoardNumberDesc(String trendBoardTitle);
-	TrendBoardEntity findByTrendBoardNumber(Integer trendBoardNumber);
-	boolean existsByTrendBoardNumber(Integer trendBoardNumber);
-=======
     
 	boolean existsByTrendBoardNumber(Integer trendBoardNumber);
 
@@ -23,5 +17,4 @@ public interface TrendBoardRepository extends JpaRepository<TrendBoardEntity, In
     List<TrendBoardEntity> findByOrderByTrendBoardNumberDesc();
     List<TrendBoardEntity> findByTrendBoardTitleOrderByTrendBoardNumberDesc(String trendBoardTitle);
     
->>>>>>> 8233bf14626911f72de1b65a4c0c6356778ad8a4
 }
--- src/main/java/com/back/back/service/TrendBoardImageService.java
+++ src/main/java/com/back/back/service/TrendBoardImageService.java
@@ -4,13 +4,8 @@ import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface TrendBoardImageService {
-<<<<<<< HEAD
-	String upload(MultipartFile file);
-	Resource getFile(String fileName);
-=======
 
 	String upload(MultipartFile file);
 	Resource getFile(String fileName);
 	
->>>>>>> 8233bf14626911f72de1b65a4c0c6356778ad8a4
 }
