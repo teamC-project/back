8a268572562de2600b9df7fb5c36a39801e7df9b
ahndb <ahndb.dev@gmail.com>
2024-06-26 10:27:24 +0900

    Update common util

--- src/main/java/com/back/back/common/util/ChangeDateFormatUtil.java
+++ src/main/java/com/back/back/common/util/ChangeDateFormatUtil.java
@@ -7,11 +7,12 @@ public class ChangeDateFormatUtil {
 
     public static String changeYYYYMMDDHHMM(String original) throws Exception {
 
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    Date datetime = simpleDateFormat.parse(original);
-    simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm");
-    String writeDatetime = simpleDateFormat.format(datetime);
-    return writeDatetime;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date datetime = simpleDateFormat.parse(original);
+        simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm");
+        String writeDatetime = simpleDateFormat.format(datetime);
+        return writeDatetime;
+
     }
 
     public static String changeListDateFormat(String original) throws Exception {
@@ -20,13 +21,18 @@ public class ChangeDateFormatUtil {
         Date datetime = inputFormat.parse(original);
         Date today = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
         if (dateFormat.format(datetime).equals(dateFormat.format(today))) {
+
             SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
             return timeFormat.format(datetime);
+
         } else {
+            
             SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy.MM.dd");
             return outputFormat.format(datetime);
         }
+
     }
 
     public static String getCurrentDatetiem() {
@@ -35,4 +41,5 @@ public class ChangeDateFormatUtil {
         Date now = new Date();
         return simpleDateFormat.format(now);
     }
+
 }
